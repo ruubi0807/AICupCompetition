84045b5f3a25f5ca4ff34e220d855f74e6464026
ruubi0807 <69136375+ruubi0807@users.noreply.github.com>
2024-11-13 15:57:35 +0800

    data_preprocess.py

--- Preprocess/data_preprocess.py
+++ Preprocess/data_preprocess.py
@@ -2,8 +2,8 @@ import re
 import json
 import os
 from tqdm import tqdm
-import pdfplumber
-import tabula
+import pdfplumber # 用於從PDF文件中提取文字的工具
+import tabula # 用於finance的報表資料
 
 # 載入參考資料，返回一個字典，key為檔案名稱，value為PDF檔內容的文本
 def load_data(source_path, category):
@@ -11,14 +11,15 @@ def load_data(source_path, category):
   corpus_dict = {int(file.replace('.pdf', '')): read_pdf(os.path.join(source_path, file), category) for file in tqdm(masked_file_ls)}  # 讀取每個PDF文件的文本，並以檔案名作為鍵，文本內容作為值存入字典
   return corpus_dict
 
-def table_to_string(df):
+def table_to_string(page):
   table_str = ''
-  df = df.values
-  for i in range(1, len(df)):
-    for j in range(1, len(df[0])):
-      table_str += f'{df[0][j]} {df[i][0]} {df[i][j]} '
+  page = page.values
+  for i in range(1, len(page)):
+    for j in range(1, len(page[0])):
+      table_str += f'{page[0][j]} {page[i][0]} {page[i][j]} '
   return table_str
 
+# 讀取單個PDF文件並返回其文本內容
 def read_pdf(pdf_loc, category, page_infos: list = None):
   pdf = pdfplumber.open(pdf_loc)  # 打開指定的PDF文件
 
@@ -30,13 +31,15 @@ def read_pdf(pdf_loc, category, page_infos: list = None):
     if text:
       pdf_text += text
   pdf.close()  # 關閉PDF文件
-    
+
+  # 如果讀的是finance的PDF，則多加入使用tabula處理得到的字串
   if category == 'finance':
-    dfs = tabula.read_pdf(pdf_loc, pages = 'all', multiple_tables = True)
-    tables_as_string = ''
-    for i, df in enumerate(dfs):
-      tables_as_string += table_to_string(df)
-    pdf_text += tables_as_string
+    pages = tabula.read_pdf(pdf_loc, pages = 'all', multiple_tables = True) # 讀取所有頁面
+    pdf_table = ''
+    for _, page in enumerate(pages): # 迴圈遍歷每一頁
+      pdf_table += table_to_string(page)
+      
+    pdf_text += pdf_table # 將使用pdfplumber處理得到的字串和使用tabula處理得到的字串合併
 
     return pdf_text  # 返回萃取出的文本
 
@@ -45,10 +48,12 @@ if __name__ == '__main__':
   
   source_path_insurance = os.path.join(source_path, 'insurance')  # 設定參考資料路徑
   corpus_dict_insurance = load_data(source_path_insurance, 'insurance')
+  # 將corpus_dict_insurance結果存為insurance.json，可供重複使用
   with open('insurance.json', 'w', encoding='utf-8') as file:
-    json.dump(corpus_dict_finance, file, ensure_ascii = False, indent = 4)
+    json.dump(corpus_dict_insurance, file, ensure_ascii = False, indent = 4)
         
   source_path_finance = os.path.join(source_path, 'finance')  # 設定參考資料路徑
   corpus_dict_finance = load_data(source_path_finance, 'finance')
+  # 將corpus_dict_finance結果存為finance.json，可供重複使用
   with open('finance.json', 'w', encoding='utf-8') as file:
     json.dump(corpus_dict_finance, file, ensure_ascii = False, indent = 4)
